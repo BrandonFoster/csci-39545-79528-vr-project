242bc2d7d289cbf6282986cc7a6f1ae6ee448c5b
NapatKarm <napat.karm@gmail.com>
2021-03-23 17:58:14 -0400

    Implemented Maze, snap turn, teleport

---
+++ UDU-U/Assets/SnapTurn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class SnapTurn : MonoBehaviour
+{
+    public XRNode inputSource;
+    public XRRig inputRig;
+    private float curVAL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curVAL = 0;     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 inputAxis);
+
+        if ((inputAxis[0] > 0.3) && (curVAL <= 0.3))
+        {
+            Debug.Log("Should be turning right");
+            inputRig.RotateAroundCameraUsingRigUp(45);
+            curVAL = inputAxis[0];
+        }
+        else if (((inputAxis[0] < -0.3) && (curVAL >= -0.3)))
+        {
+            Debug.Log("Should be turning left");
+            inputRig.RotateAroundCameraUsingRigUp(-45);
+            curVAL = inputAxis[0];
+        }
+        curVAL = inputAxis[0];
+    }
+}
