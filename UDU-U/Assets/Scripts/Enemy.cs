03cfe07b0f569998aef74fcd885a808b82be1635
Brian Guo <brianguo810@gmail.com>
2021-04-01 23:18:09 -0400

    Shooter/Spawning Functionality

---
+++ UDU-U/Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float minSpeed = 1f;
+    public float maxSpeed = 3f;
+    public Transform target = null;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target);
+
+            speed = Random.Range(minSpeed, maxSpeed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+
+    public void SetTarget(GameObject newTarget)
+    {
+        target = newTarget.transform;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ UDU-U/Assets/Scripts/Spawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Enemy enemyPrefab;
+    private GameObject player;
+
+    public float minXpos = -8f;
+    public float maxXpos = 7.5f;
+    public float minYpos = 2f;
+    public float maxYpos = 5f;
+    private float spawnXpos;
+    private float spawnYpos;
+
+    public float minTime = 0f;
+    public float maxTime = 2f;
+    private float timer = 0f;
+    private float spawnCounter;
+
+    bool firstSpeedUp = false;
+    bool secondSpeedUp = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountdownAndSpawn();
+    }
+
+    private void CountdownAndSpawn()
+    {
+        spawnCounter -= Time.deltaTime;
+        if(spawnCounter <= 0f)
+        {
+            SpawnEnemy();
+            spawnCounter = Random.Range(minTime, maxTime);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        spawnXpos = Random.Range(minXpos, maxXpos);
+        spawnYpos = Random.Range(minYpos, maxYpos);
+
+        (Instantiate(enemyPrefab, new Vector3(spawnXpos, spawnYpos, transform.position.z), Quaternion.identity)).SetTarget(player);
+
+    }
+}
