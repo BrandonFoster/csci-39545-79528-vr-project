9f7413ec2c8670bf4219ab33bbcebebc3826825a
NapatKarm <napat.karm@gmail.com>
2021-04-01 20:01:16 -0400

    Pre-Midterm state

---
+++ UDU-U/Assets/CollisionCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject key;
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Test SHOULD BE TRIGGER, colliding with: " + col.gameObject.name);
+        if (col.gameObject.name == key.name)
+        {
+            door.SetActive(false);
+            key.SetActive(false);
+        }
+    }
+}
--- UDU-U/Assets/SnapTurn.cs
+++ UDU-U/Assets/SnapTurn.cs
@@ -7,11 +7,13 @@ public class SnapTurn : MonoBehaviour
 {
     public XRNode inputSource;
     public XRRig inputRig;
+    public XRInteractorLineVisual lineRendering;
     private float curVAL;
     // Start is called before the first frame update
     void Start()
     {
-        curVAL = 0;     
+        curVAL = 0;
+        lineRendering.enabled = false;
     }
 
     // Update is called once per frame
@@ -19,19 +21,19 @@ public class SnapTurn : MonoBehaviour
     {
         InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
         device.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 inputAxis);
-
+        device.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggered);
         if ((inputAxis[0] > 0.3) && (curVAL <= 0.3))
         {
-            Debug.Log("Should be turning right");
             inputRig.RotateAroundCameraUsingRigUp(45);
             curVAL = inputAxis[0];
         }
         else if (((inputAxis[0] < -0.3) && (curVAL >= -0.3)))
         {
-            Debug.Log("Should be turning left");
             inputRig.RotateAroundCameraUsingRigUp(-45);
             curVAL = inputAxis[0];
         }
+        if (triggered) lineRendering.enabled = true;
+        else lineRendering.enabled = false;
         curVAL = inputAxis[0];
     }
 }
