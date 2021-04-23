b3a7a993657ef9e50b21a50aa743f9c80177b15a
Brian Guo <brianguo810@gmail.com>
2021-04-23 19:15:26 -0400

    Shoot/Death SoundFX Implementation

--- UDU-U/Assets/Scripts/Gun.cs
+++ UDU-U/Assets/Scripts/Gun.cs
@@ -7,9 +7,11 @@ public class Gun : MonoBehaviour
     public float speed = 40;
     public GameObject bullet;
     public Transform barrel;
+    public AudioClip shootSound;
 
     public void Fire()
     {
+        AudioSource.PlayClipAtPoint(shootSound, transform.position, 0.5f);
         GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
         spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
         Destroy(spawnedBullet, 2);
