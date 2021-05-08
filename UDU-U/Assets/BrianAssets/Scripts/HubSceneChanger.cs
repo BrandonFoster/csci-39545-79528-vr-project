b8621029c6d0114c621803b3b89d1eb3c6f6a585
Brian Guo <brianguo810@gmail.com>
2021-05-08 13:30:11 -0400

    HubSceneLoader Implementation

---
+++ UDU-U/Assets/BrianAssets/Scripts/HubSceneChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubSceneChanger : MonoBehaviour
+{
+    public float delayInSeconds = 2f;
+
+    public void LoadScene(string name)
+    {
+        StartCoroutine(WaitAndLoad(name));
+    }
+
+    IEnumerator WaitAndLoad(string name)
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene(name);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        LoadScene("Hub");
+    }
+}
